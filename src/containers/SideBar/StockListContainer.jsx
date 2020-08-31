5d5dca1089edb9a09f6224190d9570467b642a0b
ilacilac <qkralswl307@gmail.com>
2020-08-31 13:50:24 +0900

    merge

--- src/containers/SideBar/StockListContainer.jsx
+++ src/containers/SideBar/StockListContainer.jsx
@@ -1,35 +1,47 @@
-import React, { useCallback, useState } from 'react';
-import StockList from '../../components/SideBar/StockList';
-import { useSelector, useDispatch } from 'react-redux';
-import { getSideBarStockSagaActionCreator } from '../../redux/modules/sidebarstock';
-
+import React, { useCallback, useState } from "react";
+import StockList from "../../components/SideBar/StockList";
+import { useSelector, useDispatch } from "react-redux";
+import { getSideBarStockSagaActionCreator } from "../../redux/modules/sidebarstock";
 
 export default function StockListContainer({ search, sort }) {
-  const loading = useSelector(state => state.sideBarStock.loading)
-  let stockList = useSelector(state => state.sideBarStock.sideBarStock)
+  const loading = useSelector((state) => state.sideBarStock.loading);
+  let stockList = useSelector((state) => state.sideBarStock.sideBarStock);
 
   console.log(stockList);
 
-  if (sort === 'name') {
-    stockList = stockList.sort((a, b) => a.symbol > b.symbol ? 1 : a.symbol < b.symbol ? -1 : 0);
-    console.log(useSelector(state => state.sideBarStock.sideBarStock));
-  } else if (sort === 'cheap') {
+  if (sort === "name") {
+    stockList = stockList.sort((a, b) =>
+      a.symbol > b.symbol ? 1 : a.symbol < b.symbol ? -1 : 0
+    );
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    console.log(useSelector((state) => state.sideBarStock.sideBarStock));
+  } else if (sort === "cheap") {
     stockList = stockList.sort((a, b) => {
-      return a.stockData[Object.keys(a.stockData)[0]]["1. open"] - b.stockData[Object.keys(b.stockData)[0]]["1. open"]
-    })
-  } else if (sort === 'expensive') {
-    stockList = stockList.sort((a, b) => b.stockData[Object.keys(b.stockData)[0]]["1. open"] - a.stockData[Object.keys(a.stockData)[0]]["1. open"])
+      return (
+        a.stockData[Object.keys(a.stockData)[0]]["1. open"] -
+        b.stockData[Object.keys(b.stockData)[0]]["1. open"]
+      );
+    });
+  } else if (sort === "expensive") {
+    stockList = stockList.sort(
+      (a, b) =>
+        b.stockData[Object.keys(b.stockData)[0]]["1. open"] -
+        a.stockData[Object.keys(a.stockData)[0]]["1. open"]
+    );
   }
-
-
 
   const dispatch = useDispatch();
   const getsidebarStock = useCallback(() => {
     dispatch(getSideBarStockSagaActionCreator(search));
-  }, [dispatch, search])
+  }, [dispatch, search]);
 
-
-  return (<StockList getsidebarStock={getsidebarStock} loading={loading} search={search} stockList={stockList} sort={sort} />)
-
-
+  return (
+    <StockList
+      getsidebarStock={getsidebarStock}
+      loading={loading}
+      search={search}
+      stockList={stockList}
+      sort={sort}
+    />
+  );
 }
--- src/redux/middlewares/saga.js
+++ src/redux/middlewares/saga.js
@@ -1,18 +1,8 @@
-import {
-  all
-} from "redux-saga/effects";
-import {
-  stockSaga
-} from "../modules/stock";
-import {
-  DJIASaga
-} from "../modules/djia";
-import {
-  sideBarStockSaga
-} from "../modules/sidebarstock";
-
-
+import { all } from "redux-saga/effects";
+import { stockSaga } from "../modules/stock";
+import { DJIASaga } from "../modules/djia";
+import { sideBarStockSaga } from "../modules/sidebarstock";
 
 export default function* rootSaga() {
-  yield all([stockSaga(), sideBarStockSaga(), DJIASaga()])
+  yield all([stockSaga(), sideBarStockSaga(), DJIASaga()]);
 }
