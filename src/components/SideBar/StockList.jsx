e8652d7ae65038b74fc6abe28ad2261604864d27
jihyeon57 <ehwlgus22@gmail.com>
2020-09-02 21:26:19 +0900

    symbol save localstorage

--- src/components/SideBar/StockList.jsx
+++ src/components/SideBar/StockList.jsx
@@ -1,51 +1,81 @@
-import React, { useEffect } from 'react';
-import * as V from 'victory';
-import { createChart } from 'lightweight-charts';
+import React, { useEffect, useRef } from "react";
+import * as V from "victory";
+import { createChart } from "lightweight-charts";
 
-
-
-export default function StockList({ stockList, getsidebarStock, loading, search, menu }) {
+export default function StockList({
+  stockList,
+  getsidebarStock,
+  loading,
+  search,
+  menu,
+}) {
   useEffect(() => {
-    getsidebarStock(search)
-  }, [getsidebarStock, search])
+    getsidebarStock(search);
+  }, [getsidebarStock, search]);
 
+  // if (stockList.length !== 0) {
+  //   const a = Object.values(stockList[0]);
+  //   console.log(a[0]);
+  // }
+  const selectedItem = useRef();
 
+  const clickList = () => {
+    // console.log(selectedItem.current.firstChild);
 
+    if (stockList.length !== 0) {
+      const selectedSymbol = selectedItem.current.firstChild;
+      let b = [];
+      stockList.map((stock, i) => {
+        // return Object.values(stock[i]) === selectedSymbol
+        //   ? selectedSymbol
+        //   : clickList();
+        console.log(stock, i);
+        const a = Object.values(stock);
+        b[i] += "" + a[0];
+        // console.log(b);
+        // const c = b[i] === selectedSymbol ? selectedSymbol : "";
+      });
+    }
+  };
 
   if (!loading) {
     return (
       <div className="stock-sidebar">
         <ul className={menu ? "" : "none"}>
+          {stockList.map(
+            (stock) => {
+              let stocks = [];
+              const keys = Object.keys(stock.stockData).reverse();
+              const values = Object.values(stock.stockData)
+                .map((item) => +item["1. open"])
+                .reverse();
+              keys.forEach((item, i) => {
+                stocks.push({ date: item, price: values[i] });
+              });
+              let color = stock.change[0] === "-" ? "yellow" : "red";
 
-          {stockList.map(stock => {
-            let stocks = []
-            const keys = Object.keys(stock.stockData).reverse()
-            const values = Object.values(stock.stockData).map(item => +item["1. open"]).reverse()
-            keys.forEach((item, i) => { stocks.push({ date: item, price: values[i] }) })
-            let color = stock.change[0] === "-" ? "yellow" : "red"
-
-
-            return <li>
-              {stock.change}
-              {stock.symbol}
-              {stock.name}
-              <V.VictoryLine
-                data={stocks}
-                x="date"
-                y="price"
-                style={{
-                  data: { stroke: color },
-                  parent: {
-                    width: 50,
-                    height: "auto"
-                  }
-
-                }}
-              />
-
-
-            </li>
-          }
+              return (
+                <li onClick={clickList}>
+                  <span ref={selectedItem}>
+                    {stock.symbol}
+                    {stock.change}
+                    {stock.name}
+                  </span>
+                  <V.VictoryLine
+                    data={stocks}
+                    x="date"
+                    y="price"
+                    style={{
+                      data: { stroke: color },
+                      parent: {
+                        width: 50,
+                        height: "auto",
+                      },
+                    }}
+                  />
+                </li>
+              );
+            }
 
             // <li><Plot
             //   data={[
@@ -58,19 +88,15 @@ export default function StockList({ stockList, getsidebarStock, loading, search,
             //   ]}
             //   layout={{ width: 400, height: 250, showlegend: false, modebar: false, displaymodebar: false }}
 
-
             // />
             //   {stock.change}
             //   {stock.symbol}
             // </li>)
-          )
-          }
+          )}
         </ul>
       </div>
-    )
+    );
   }
 
-
-  return <h1>hi</h1>
-
+  return <h1>hi</h1>;
 }
