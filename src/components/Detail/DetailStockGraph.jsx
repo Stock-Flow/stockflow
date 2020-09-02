9595b5669e5e0dd31e23199d16b59900f0ef2913
ilacilac <qkralswl307@gmail.com>
2020-09-03 01:28:45 +0900

    feat: Changing detail stock graph

--- src/components/Detail/DetailStockGraph.jsx
+++ src/components/Detail/DetailStockGraph.jsx
@@ -1,17 +1,51 @@
-import React from "react";
-import Plot from "react-plotly.js";
-import { useEffect } from "react";
-import { dispatch } from "d3";
+import React, { useRef } from 'react';
+import Plot from 'react-plotly.js';
+import { useEffect } from 'react';
+import { dispatch } from 'd3';
+import { createChart } from 'lightweight-charts';
+
 // loading
-export default function DetailStockGraph({ getDetailStock, loading, symbol, intraBtnClick, dailyBtnClick, weeklyBtnClick, monthlyBtnClick, stockData }) {
-
+export default function DetailStockGraph({
+  getDetailStock,
+  loading,
+  symbol,
+  intraBtnClick,
+  dailyBtnClick,
+  weeklyBtnClick,
+  monthlyBtnClick,
+  stock,
+  func,
+}) {
+  const chart = useRef();
+  const chartposition = useRef();
   useEffect(() => {
-    getDetailStock("TIME_SERIES_DAILY", "IBM");
-  }, [getDetailStock]);
-
+    if (chartposition.current) {
+      chart.current = createChart(chartposition.current, {
+        width: 800,
+        height: 400,
+      });
+      chart.current.applyOptions({
+        priceScale: {
+          position: 'left',
+          autoScale: true,
+        },
+        timeScale: {
+          fixLeftEdge: true,
+          barSpacing: 10,
+        },
+      });
+      if (chart.current) {
+        const lineSeries = chart.current.addCandlestickSeries({
+          title: 'DOW J',
+        });
+        lineSeries.setData(stock);
+      }
+    }
+  }, [stock]);
+  // stock
+  // 0: {time: "2020-04-13", open: 121.63, high: 121.8, low: 118.04, close: 121.15}
 
   return (
-
     <>
       <h1>Detail Stock</h1>
       {!loading && (
@@ -21,7 +55,7 @@ export default function DetailStockGraph({ getDetailStock, loading, symbol, intr
           <button onClick={() => weeklyBtnClick()}>1주</button>
           <button onClick={() => monthlyBtnClick()}>1달</button>
           <ul>
-            <li>
+            {/* <li>
               <Plot
                 data={[
                   {
@@ -35,9 +69,36 @@ export default function DetailStockGraph({ getDetailStock, loading, symbol, intr
                 ]}
                 layout={{ width: 800, height: 350 }}
               />
-            </li>
+            </li> */}
             {/* title: symbol */}
           </ul>
+
+          <div ref={chartposition}></div>
+
+          {/* <div
+            ref={(el) => {
+              chart.current = createChart(el, {
+                width: 800,
+                height: 400,
+              });
+              chart.current.applyOptions({
+                priceScale: {
+                  position: 'left',
+                  autoScale: true,
+                },
+                timeScale: {
+                  fixLeftEdge: true,
+                  barSpacing: 10,
+                },
+              });
+              if (chart.current) {
+                const lineSeries = chart.current.addCandlestickSeries({
+                  title: 'stock',
+                });
+                lineSeries.setData(stock);
+              }
+            }}
+          ></div> */}
         </>
       )}
     </>
@@ -45,4 +106,3 @@ export default function DetailStockGraph({ getDetailStock, loading, symbol, intr
 
   // return <h1>Detail Stock</h1>;
 }
-
--- src/services/DetailStockService.js
+++ src/services/DetailStockService.js
@@ -1,14 +1,24 @@
-import axios from "axios";
-import {
-  apiKey
-} from "../key";
+import axios from 'axios';
+import { apiKey } from '../key';
 
 export default class DetailStockService {
-  static async getStockDaily(func, symbol) {
+  static async getStockDaily(func, symbol, date) {
     const stockData = await axios.get(
-      `https://www.alphavantage.co/query?function=${func}&symbol=${symbol}&interval=1min&apikey=${apiKey}`
+      `https://www.alphavantage.co/query?function=${func}&symbol=${symbol}&interval=1min&apikey=${apiKey}`,
     );
 
-    return stockData.data;
+    let newDateForm = stockData.data[date];
+    let newDateFormArr = [];
+    for (let i in newDateForm) {
+      newDateFormArr.push({
+        time: i,
+        open: Number(newDateForm[i]['1. open']),
+        high: Number(newDateForm[i]['2. high']),
+        low: Number(newDateForm[i]['3. low']),
+        close: Number(newDateForm[i]['4. close']),
+      });
+    }
+    newDateFormArr = newDateFormArr.reverse();
+    return newDateFormArr;
   }
 }
