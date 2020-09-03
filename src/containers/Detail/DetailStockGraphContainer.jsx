f457847d6b645f570637530eabc170db4bdab179
SaltPepperSalt <dksro324@gmail.com>
2020-09-03 13:04:45 +0900

    feat: Moving Average

--- src/containers/Detail/DetailStockGraphContainer.jsx
+++ src/containers/Detail/DetailStockGraphContainer.jsx
@@ -3,10 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import DetailStockGraph from '../../components/Detail/DetailStockGraph';
 import { getDetailStockSagaActionCreator } from '../../redux/modules/detailStock';
 import { useState } from 'react';
-import DetailStockService from '../../services/DetailStockService';
 
 export default function DetailStockGraphContainer({
-  func = 'TIME_SERIES_DAILY',
+  func = 'TIME_SERIES_DAILY_ADJUSTED',
   symbol = 'IBM',
 }) {
   const loading = useSelector((state) => state.detailStock.loading);
@@ -14,43 +13,90 @@ export default function DetailStockGraphContainer({
   const [date, setDate] = useState('Time Series (Daily)');
 
   const dispatch = useDispatch();
-  const getDetailStock = useCallback(() => {
+  const getDetailStock = useCallback((func, symbol, date) => {
     dispatch(
-      getDetailStockSagaActionCreator(func, symbol, 'Time Series (Daily)'),
+      getDetailStockSagaActionCreator(func, symbol, date),
     );
-  }, []);
+  }, [dispatch]);
 
-  let stockData = stock[date];
+  const movingAverageFive = (stock) => {
+    const movingAverage = []
+    for (let i = 0; i < stock.length; i += 5) {
+      let sum = 0;
+      for (let j = 0; j < 5; j++) {
+        sum += +stock[i + j].close
 
-  function dailyBtnClick(func, symbol) {
-    setDate('Time Series (Daily)');
-    func = 'TIME_SERIES_DAILY';
-    symbol = 'IBM';
-    dispatch(
-      getDetailStockSagaActionCreator(func, symbol, 'Time Series (Daily)'),
-    );
+      }
+      movingAverage.push({ time: stock[i].time, value: sum / 5 })
+    }
+    movingAverage.push({ time: stock[stock.length - 1].time, value: stock[stock.length - 1].close })
+    return movingAverage
   }
-  function weeklyBtnClick(func, symbol) {
-    setDate('Weekly Time Series');
-    func = 'TIME_SERIES_WEEKLY';
-    symbol = 'IBM';
-    dispatch(
-      getDetailStockSagaActionCreator(func, symbol, 'Weekly Time Series'),
-    );
-  }
-  function monthlyBtnClick(func, symbol) {
-    setDate('Monthly Time Series');
-    func = 'TIME_SERIES_MONTHLY';
-    symbol = 'IBM';
-    dispatch(
-      getDetailStockSagaActionCreator(func, symbol, 'Monthly Time Series'),
-    );
+  const movingAverageTen = (stock) => {
+    const movingAverage = []
+    for (let i = 0; 10 * (Math.trunc(stock.length / 10)); i += 10) {
+      let sum = 0;
+      for (let j = 0; j < 10; j++) {
+        sum += +stock[i + j].close
 
-  
+      }
+      movingAverage.push({ time: stock[i].time, value: sum / 10 })
+    }
+    movingAverage.push({ time: stock[stock.length - 1].time, value: stock[stock.length - 1].close })
+    return movingAverage
   }
-  useEffect(() => {
-    getDetailStock(func, symbol, 'Time Series (Daily)');
-  }, []);
+  const movingAverageTwenty = (stock) => {
+    const movingAverage = []
+    for (let i = 0; i < 20 * (Math.trunc(stock.length / 20)); i += 20) {
+      let sum = 0;
+      for (let j = 0; j < 20; j++) {
+        sum += +stock[i + j].close
+
+      }
+      movingAverage.push({ time: stock[i].time, value: sum / 20 })
+    }
+    movingAverage.push({ time: stock[stock.length - 1].time, value: stock[stock.length - 1].close })
+    return movingAverage
+  }
+  const movingAverageSixty = (stock) => {
+    const movingAverage = []
+    for (let i = 0; i < 60 * (Math.trunc(stock.length / 60)); i += 60) {
+      let sum = 0;
+      for (let j = 0; j < 60; j++) {
+        sum += +stock[i + j].close
+
+      }
+      movingAverage.push({ time: stock[i].time, value: sum / 60 })
+    }
+    movingAverage.push({ time: stock[stock.length - 1].time, value: stock[stock.length - 1].close })
+    return movingAverage
+  }
+  // function dailyBtnClick(func, symbol) {
+  //   setDate('Time Series (Daily)');
+  //   func = 'TIME_SERIES_DAILY';
+  //   symbol = 'IBM';
+  //   dispatch(
+  //     getDetailStockSagaActionCreator(func, symbol, 'Time Series (Daily)'),
+  //   );
+  // }
+  // function weeklyBtnClick(func, symbol) {
+  //   setDate('Weekly Time Series');
+  //   func = 'TIME_SERIES_WEEKLY_ADJUSTED';
+  //   symbol = 'IBM';
+  //   dispatch(
+  //     getDetailStockSagaActionCreator(func, symbol, 'Weekly Adjusted Time Series'),
+  //   );
+  // }
+  // function monthlyBtnClick(func, symbol) {
+  //   setDate('Monthly Time Series');
+  //   func = 'TIME_SERIES_MONTHLY_ADJUSTED';
+  //   symbol = 'IBM';
+  //   dispatch(
+  //     getDetailStockSagaActionCreator(func, symbol, 'Monthly Adjusted Time Series'),
+  //   );
+
+
+  // }
 
   // console.log(useSelector((state) => state));
 
@@ -58,13 +104,15 @@ export default function DetailStockGraphContainer({
   return (
     <DetailStockGraph
       getDetailStock={getDetailStock}
+      movingAverageFive={movingAverageFive}
+      movingAverageTen={movingAverageTen}
+      movingAverageTwenty={movingAverageTwenty}
+      movingAverageSixty={movingAverageSixty}
       loading={loading}
       stock={stock}
       func={func}
+      date={date}
       symbol={symbol}
-      dailyBtnClick={dailyBtnClick}
-      weeklyBtnClick={weeklyBtnClick}
-      monthlyBtnClick={monthlyBtnClick}
     />
   );
 }
--- src/services/DetailStockService.js
+++ src/services/DetailStockService.js
@@ -1,24 +1,19 @@
 import axios from 'axios';
-import { apiKey } from '../key';
+import {
+  apiKey
+} from '../key';
+import DataProcessingService from './DataProcessingService';
 
 export default class DetailStockService {
   static async getStockDaily(func, symbol, date) {
     const stockData = await axios.get(
-      `https://www.alphavantage.co/query?function=${func}&symbol=${symbol}&interval=1min&apikey=${apiKey}`,
+      `https://www.alphavantage.co/query?function=${func}&symbol=${symbol}&outputsize=full&apikey=${apiKey}`,
     );
 
-    let newDateForm = stockData.data[date];
-    let newDateFormArr = [];
-    for (let i in newDateForm) {
-      newDateFormArr.push({
-        time: i,
-        open: Number(newDateForm[i]['1. open']),
-        high: Number(newDateForm[i]['2. high']),
-        low: Number(newDateForm[i]['3. low']),
-        close: Number(newDateForm[i]['4. close']),
-      });
-    }
-    newDateFormArr = newDateFormArr.reverse();
-    return newDateFormArr;
+    let detailStock = DataProcessingService.DataProcessing(stockData.data, date)
+
+    detailStock = DataProcessingService.AdjustSplitSingle(detailStock, date)
+    detailStock = DataProcessingService.GraphDataProcessing(detailStock);
+    return detailStock;
   }
 }
