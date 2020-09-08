110967a1525f9389658213a5f234a6023c48255f
jihyeon57 <ehwlgus22@gmail.com>
2020-09-08 21:22:51 +0900

    pmorigin pull

--- src/containers/MainDjia/djiagraphContainer.jsx
+++ src/containers/MainDjia/djiagraphContainer.jsx
@@ -1,14 +1,11 @@
-
-import React from "react";
-import { useSelector } from "react-redux";
-import DjiaGraph from "../../components/MainDjia/djiagraph";
-import { useEffect } from "react";
-import "../../components/MainDjia/MainDjia.scss";
-import DataProcessingService from "../../services/DataProcessingService";
-
+import React from 'react';
+import { useSelector } from 'react-redux';
+import DjiaGraph from '../../components/MainDjia/djiagraph';
+import { useEffect } from 'react';
+import '../../components/MainDjia/MainDjia.scss';
+import DataProcessingService from '../../services/DataProcessingService';
 
 export default function DjiagraphContainer() {
-  const DOW_DIVISOR = 0.14748071991788;
   const djia = useSelector((state) => state.djia.djia);
   // const sum = useEffect(() => {
   //   sumOpenData()
@@ -24,11 +21,11 @@ export default function DjiagraphContainer() {
   let djiaDate = [];
 
   if (djia.length !== 0) {
-    let djiaOpenData = []
+    let djiaOpenData = [];
     let djiaHighData = [];
     let djiaLowData = [];
     let djiaCloseData = [];
-    djiaDate = djiaStockData[0].map(date => date.time)
+    djiaDate = djiaStockData[0].map((date) => date.time);
     for (let i = 0; i < djiaStockData.length; i++) {
       for (let j = 0; j < djiaStockData[0].length; j++) {
         if (i === 0) {
@@ -38,7 +35,7 @@ export default function DjiagraphContainer() {
           djiaCloseData.push(0);
         }
         // console.log(djiaStockData[i][j].open)
-        djiaOpenData[j] += +djiaStockData[i][j].open
+        djiaOpenData[j] += +djiaStockData[i][j].open;
         djiaHighData[j] += +djiaStockData[i][j].high;
         djiaLowData[j] += +djiaStockData[i][j].low;
         djiaCloseData[j] += +djiaStockData[i][j].close;
@@ -49,7 +46,6 @@ export default function DjiagraphContainer() {
     djiaLowData = DataProcessingService.GetDJiaProcessing(djiaLowData);
     djiaCloseData = DataProcessingService.GetDJiaProcessing(djiaCloseData);
     djiaList = [djiaOpenData, djiaHighData, djiaLowData, djiaCloseData];
-
   }
 
   return (
