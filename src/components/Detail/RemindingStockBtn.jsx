0cb2ef8ca69a997825cb33c04455ca5270289acb
ilacilac <qkralswl307@gmail.com>
2020-09-11 17:54:10 +0900

    feat: Reminding Stock Alert -Getting stocklist When the volume is bigger 1 day before than 2 days ago -scss

---
+++ src/components/Detail/RemindingStockBtn.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+export default function RemindingStockBtn() {
+  // const [remindingStock, setRemindingStock] = useState(false);
+
+  // function click() {
+  //   setRemindingStock(!remindingStock);
+  // }
+  // return (
+  //   <button className="reminding-btn" onClick={click}>
+  //     알림
+  //   </button>
+  // );
+  return <></>;
+}
