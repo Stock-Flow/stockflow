482098a01440f2bbcd6e8b5e81129ca1e7e81e25
ilacilac <qkralswl307@gmail.com>
2020-09-13 23:06:02 +0900

    feat: Reminding stock - alert popup click event - Showing alert count

--- src/containers/Detail/RemindingStockContainer.jsx
+++ src/containers/Detail/RemindingStockContainer.jsx
@@ -6,11 +6,23 @@ import { useState } from 'react';
 
 export default function () {
   const stockList = useSelector((state) => state.sideBarStock.sideBarStock);
+  const loading = useSelector((state) => state.sideBarStock.loading);
+  const remindinStockList = stockList.filter((stock) => {
+    const volume = stock.stockData.map((item) => item.volume).reverse();
+    if (
+      !(volume[2] < volume[1] && volume[1] < volume[0]) ||
+      // !(volume[1] < volume[0]) ||
+      stock.symbol === 'DOW'
+    ) {
+      return false;
+    }
+    return stock;
+  });
 
   return (
     <div className="reminding-stock-wrap">
       <RemindingStockBtn />
-      <RemindingStock stockList={stockList} />
+      <RemindingStock remindinStockList={remindinStockList} loading={loading} />
     </div>
   );
 }
