024202a1a60397635e64efafbd8fc7bddd2817e0
ilacilac <qkralswl307@gmail.com>
2020-09-14 22:26:50 +0900

    fix: Reminding stock - Changin naming remindinStockList => remindingStockList - when alert message was cliked, Do not changing state

--- src/containers/Detail/RemindingStockContainer.jsx
+++ src/containers/Detail/RemindingStockContainer.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import RemindingStock from '../../components/Detail/RemindingStock';
 import { useSelector } from 'react-redux';
 import RemindingStockBtn from '../../components/Detail/RemindingStockBtn';
-import { useState } from 'react';
 
 export default function () {
   const stockList = useSelector((state) => state.sideBarStock.sideBarStock);
   const loading = useSelector((state) => state.sideBarStock.loading);
-  const remindinStockList = stockList.filter((stock) => {
+  const remindingStockList = stockList.filter((stock) => {
     const volume = stock.stockData.map((item) => item.volume).reverse();
     if (
       !(volume[2] < volume[1] && volume[1] < volume[0]) ||
-      // !(volume[1] < volume[0]) ||
       stock.symbol === 'DOW'
     ) {
       return false;
@@ -22,7 +20,10 @@ export default function () {
   return (
     <div className="reminding-stock-wrap">
       <RemindingStockBtn />
-      <RemindingStock remindinStockList={remindinStockList} loading={loading} />
+      <RemindingStock
+        remindingStockList={remindingStockList}
+        loading={loading}
+      />
     </div>
   );
 }
