c14fffe38dc488a405dbeda783d4b45ea1559bc0
ilacilac <qkralswl307@gmail.com>
2020-09-21 15:25:34 +0900

    style: main progressbar

--- src/components/MainDjia/djiagraph.jsx
+++ src/components/MainDjia/djiagraph.jsx
@@ -12,18 +12,21 @@ export default function DjiaGraph({ djiaList, djiaDate, loading, done }) {
   const lineSeries = useRef();
   const chartposition = useRef();
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  window.addEventListener('resize', useCallback(() => {
-    setWindowWidth(window.innerWidth);
-    console.log('hi');
-    if (chart.current) {
-      if (windowWidth >= 1200) {
-        chart.current.resize(windowWidth * 0.72 - 100, 400);
+  window.addEventListener(
+    'resize',
+    useCallback(() => {
+      setWindowWidth(window.innerWidth);
+      console.log('hi');
+      if (chart.current) {
+        if (windowWidth >= 1200) {
+          chart.current.resize(windowWidth * 0.72 - 100, 400);
+        }
+        if (windowWidth < 1200) {
+          chart.current.resize(windowWidth * 0.72, 400);
+        }
       }
-      if (windowWidth < 1200) {
-        chart.current.resize(windowWidth * 0.72, 400);
-      }
-    }
-  }, [windowWidth]))
+    }, [windowWidth]),
+  );
   useEffect(() => {
     if (windowWidth > 1200) {
       chart.current = createChart(chartposition.current, {
@@ -80,14 +83,19 @@ export default function DjiaGraph({ djiaList, djiaDate, loading, done }) {
     <div className="djia">
       <h2>DOW J</h2>
       <div ref={chartposition}></div>
-      {loading ? <>
-        <h1 className="progress-percent">{(done * (100 / 30)).toFixed(0)}%</h1>
-        <progress max="30" value={done} className="djia-progress">
-        </progress> </> : <div className="foreign-exchange-wrap">
+      {loading ? (
+        <>
+          <progress max="30" value={done} className="djia-progress"></progress>{' '}
+          <div className="progress-percent">
+            Loading... {(done * (100 / 30)).toFixed(0)}%
+          </div>
+        </>
+      ) : (
+        <div className="foreign-exchange-wrap">
           <ForeignExchangeContainer />
           <ForeignExchangeDetailContainer />
-        </div>}
-
+        </div>
+      )}
     </div>
   );
 }
